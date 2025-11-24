404ae79b4e47265860f144bbb1dc9f73fe29f9de
GoDisCover <jessenco122@gmail.com>
2025-11-24 18:30:12 +0700

    progress

--- lib/widgets/film_card.dart
+++ lib/widgets/film_card.dart
@@ -4,30 +4,130 @@ class FilmCard extends StatelessWidget {
   const FilmCard({super.key});
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: GestureDetector(
-        onTap: () {
-          print('Test');
-        },
-        child: Stack(
-          children: [
-            Image.network(
-              'https://cdn.myanimelist.net/images/anime/1255/110636.jpg',
-              fit: BoxFit.cover,
-            ),
-            Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: <Color>[Color(0x0057564F), Color(0xFF57564F)],
-                  stops: [0.5, 1.0],
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadiusGeometry.circular(8),
+      ),
+      margin: const EdgeInsets.all(4),
+      elevation: 1.0,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: GestureDetector(
+                onTap: () {
+                  print('Test');
+                },
+                child: Stack(
+                  children: [
+                    Image.network(
+                      'https://cdn.myanimelist.net/images/anime/1255/110636.jpg',
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                      height: double.infinity,
+                    ),
+                    Container(
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: <Color>[Color(0x0057564F), Color(0xFF57564F)],
+                          stops: [0.5, 1.0],
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Color(0xFF57564F),
+                            borderRadius: BorderRadius.circular(9),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(3.5),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Icon(Icons.star, color: Colors.black),
+                                SizedBox(width: 4),
+                                Text("4.9/5"),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              "Yuru Camp",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 4),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 6, vertical: 2),
+                              decoration: BoxDecoration(
+                                color: Colors.orange,
+                                borderRadius: BorderRadius.circular(6),
+                              ),
+                              child: Text(
+                                "Watching",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
+    // SizedBox(
+    //   child: GestureDetector(
+    //     onTap: () {
+    //       print('Test');
+    //     },
+    //     child: Stack(
+    //       children: [
+    //         Image.network(
+    //           'https://cdn.myanimelist.net/images/anime/1255/110636.jpg',
+    //           fit: BoxFit.cover,
+    //         ),
+    //         Container(
+    //           decoration: const BoxDecoration(
+    //             gradient: LinearGradient(
+    //               begin: Alignment.topCenter,
+    //               end: Alignment.bottomCenter,
+    //               colors: <Color>[Color(0x0057564F), Color(0xFF57564F)],
+    //               stops: [0.5, 1.0],
+    //             ),
+    //           ),
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 }
