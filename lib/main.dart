9a04c53e0c2985e46b92bb3eba2b8fd15caeabda
GoDisCover <jessenco122@gmail.com>
2025-11-28 20:34:10 +0700

    NavBar Otw

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:review_film/pages/my_review_page.dart';
+import 'package:review_film/pages/profile_page.dart';
+import 'package:review_film/pages/search_page.dart';
 import 'pages/login_page.dart';
 
 void main() {
@@ -14,7 +16,44 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       // home: const LoginPage(),
-      home: const MyReviewPage()
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _curentIndex = 0;
+  final List<Widget> _children = [MyReviewPage(), SearchPage(), ProfilePage()];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _children[_curentIndex],
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(canvasColor: Colors.deepPurple),
+        child: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+          currentIndex: _curentIndex,
+          selectedItemColor: Colors.amber[800],
+          
+          showUnselectedLabels: false,
+          onTap: (index) {
+            setState(() {
+              _curentIndex = index;
+            });
+          },
+        ),
+      ),
     );
   }
 }
--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:review_film/widgets/film_card.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Review List'),
+        backgroundColor: const Color(0xFF57564F),
+      ),
+      backgroundColor: const Color(0xFF7A7A73),
+      body: Padding(
+        padding: const EdgeInsets.all(5),
+        child: GridView.builder(
+          itemCount: 6,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 12,
+            mainAxisSpacing: 12,
+            childAspectRatio: 0.7,
+          ),
+          itemBuilder: (BuildContext context, int index) {
+            return FilmCard();
+          },
+        ),
+      ),
+    );
   }
 }
